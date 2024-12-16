1dff67bcbf35a2ef5296be708ebfb0fb937269b1
Timo Humphrey <github.com@humph.in>
2024-12-16 16:35:24 -0500

    Update autoplay.py

--- autoplay.py
+++ autoplay.py
@@ -9,7 +9,7 @@ PAUSE_BETWEEN_PLAYLIST_REPLAYS = 1
 
 def get_playlist() -> list:
     try:
-        with open("./playlist") as file:
+        with open("./playlist.pl") as file:
             playlist = [line.rstrip() for line in file if is_file(line.rstrip())]
 
         if len(playlist) == 0:
